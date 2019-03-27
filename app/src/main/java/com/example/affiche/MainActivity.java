c75fe904e27c060418b1593ce3ee766c64ca144f
Art-ylem <art-ylem@mail.ru>
2019-03-27 13:12:08 +0500

    фрагмент

--- app/src/main/java/com/example/affiche/MainActivity.java
+++ app/src/main/java/com/example/affiche/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.affiche;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Fragment1.OnFragmentInteractionListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.affiche_box);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+
+        // Create new fragment and transaction
+        Fragment newFragment = Fragment1.newInstance("", "");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack
+        transaction.replace(R.id.container_for_fragments, newFragment);
+        transaction.addToBackStack(null);
+
+// Commit the transaction
+        transaction.commit();
+
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+
+                        int id = menuItem.getItemId();
+
+                        if (id == R.id.nav_camera) {
+                            // Handle the camera action
+                        } else if (id == R.id.nav_gallery) {
+                            setContentView(R.layout.affiche_box);
+
+                        } else if (id == R.id.nav_slideshow) {
+
+                        } else if (id == R.id.nav_manage) {
+
+                        }
+
+                        drawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public void onClickButton() {
+        Toast.makeText(this, "onClickButton", Toast.LENGTH_SHORT).show();
     }
 }
